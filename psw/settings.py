f851c765602b96fa538cf472443f6afb599f7308
LifeArchitect <pswinspire@live.com>
2017-07-29 13:37:01 +0800

    sidebar v1

--- psw/settings.py
+++ psw/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pswapp',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/personal_site/psw/psw/pswapp/static',
+]
